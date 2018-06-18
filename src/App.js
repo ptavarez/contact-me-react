06a168cc088eee134d520fd2af16f4a551391b8a
ptavarez16 <ptavarez16@yahoo.com>
2018-06-18 17:20:54 -0400

    Define /main route - Define route by using Switch and Route components provided by the react-router-dom module. - Users will be authenticated and redirected to the Home component content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthService from './AuthService';
+import {Switch, Route} from 'react-router-dom';
+import Home from './Home'
 
 class App extends Component {
   constructor() {
@@ -9,21 +11,34 @@ class App extends Component {
     this.authService = new AuthService();
   }
 
-  render() {
-    this.authService.login();
+  renderHome() {
+    let resultComponent = <Home auth={this.authService}/>;
 
+    if (!this.authService.isAuthenticated()) {
+      this.authService.login();
+      resultComponent = <div><p>Redirecting to the authentication service...</p></div>
+    }
+
+    return resultComponent;
+  }
+
+  startSession(history) {
+    this.authService.handleAuthentication(history);
+    return <div><p>Starting session...</p></div>;
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to ContactMe</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path="/" render={() => this.renderHome()}/>
+          <Route path="/main" render={({history}) => this.startSession(history)}/>
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
