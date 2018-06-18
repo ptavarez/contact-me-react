a7e98396ed610dc19939666111bb5d5c07673f69
ptavarez16 <ptavarez16@yahoo.com>
2018-06-18 18:34:03 -0400

    Add logout button

--- src/App.js
+++ src/App.js
@@ -26,12 +26,25 @@ class App extends Component {
     this.authService.handleAuthentication(history);
     return <div><p>Starting session...</p></div>;
   }
+  
+  createLogoutButton() {
+    let button = null;
+
+    if (this.authService.isAuthenticated()) {
+        button = <button onClick={()=>this.authService.logout()}>Logout</button>;
+    }
+
+    return button;
+  }
 
   render() {
+    let logoutButton =  this.createLogoutButton();
+    
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to ContactMe</h1>
+          {logoutButton}
         </header>
         <Switch>
           <Route exact path="/" render={() => this.renderHome()}/>
